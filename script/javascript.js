e0f19b69da9f645791f6383416795b5fed393f12
oussama-crypto <53824249+oussama-crypto@users.noreply.github.com>
2019-08-16 14:14:06 +0100

    Add files via upload

---
+++ script/javascript.js
@@ -0,0 +1,20 @@
+function myFunction1(){
+document.getElementById('para').classList.toggle('bold-class');
+    
+    
+}
+function myFunction2(){
+    document.getElementById('para').classList.toggle('italic-class');
+    
+}
+function myFunction3(){
+document.getElementById('para').classList.toggle('underlined-class');
+}
+function myFunction4(){
+    document.getElementById("para").style.fontSize=document.getElementById("mychoise").value;
+
+}
+function myFunction5(){
+    document.getElementById("para").style.fontFamily=document.getElementById("select-font").value;
+}
+   
